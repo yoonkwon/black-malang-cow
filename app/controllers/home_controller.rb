af1c1569c4e752d635cf38f3af52469323284dfe
alberto.bb <alberto.bb@kakaomobility.com>
2019-09-26 18:06:57 +0900

    190926 memo scaffold

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,6 @@
 class HomeController < ApplicationController
 
   def index
-    if user_signed_in?
-      render json: {result: current_user}
-    else
-      redirect_to user_signin_path
-    end
+    render json: {result: current_user}
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  resources :memos
+  get 'user/index'
   get 'home/index'
   devise_for :users
+  get 'users', action: :index, controller: 'user'
   get "/health", to: proc { [200, {}, ['']] }
   root to: 'home#index'
 end
